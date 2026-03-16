b71127ab7873f19e2cafa8d74dfd505cc1b98946
nebelnest1 <nebelnest1488@gmail.com>
2026-03-16 16:13:26 +0300

    Add files via upload

---
+++ assets/scripts/appSdk.js
@@ -0,0 +1,10 @@
+import {
+    setUrlSearchParam
+} from "./shared-5XR54UIB.js";
+import "./shared-IZBMCQM6.js";
+var _a;
+if (typeof window !== "undefined") {
+    window.app = Object.assign((_a = window.app) != null ? _a : {}, {
+        setUrlSearchParam
+    });
+}
